a82b27f5a18164cebc07d1431132d5b080389f7d
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-09-10 20:18:53 +0300

    ilk commit

---
+++ git.js
@@ -0,0 +1,39 @@
+/*
+//Git Nedir?
+    versiyonun ismi
+    tarih 
+    kullanıcı
+
+    versiyon2den versiyon1 e dönmek istediğimizde 
+    git işimizi görür
+
+//Linux Kurulum 
+    sudo apt-get install git
+
+//Git Yapılandırılması
+    kullanıcı adı ve mail tanıtma
+    git config --global user.name "kullanıcı adınız"
+    git config --global user.email "email adresiniz"
+
+    --global //bu sistemdeki kullanıcı
+
+//Git Projesi Oluşturma
+    Git projesi oluşturmak için şu komutu kullanmanız yeterli ;
+    git init
+    Oluşturduğunuz gizli git dosyalarını listelemek için şu komutu çalıştırabilirsiniz:
+    ls -a
+
+//Commit & Log
+proje dosyalarını git repository ' ye (git deposuna) eklemeyi  gerçekleştiriyoruz. Komutlar şu şekilde :
+git add .
+git commit -m "mesaj"
+
+Aldığımız versiyonların tamamını listelemek için şu komutu çalıştırıyoruz:
+git log
+
+
+
+
+
+
+*/
